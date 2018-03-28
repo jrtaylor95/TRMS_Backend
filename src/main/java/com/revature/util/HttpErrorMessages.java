016e13f10e10616b8ae462b7757a6b0113437a1a
James Taylor <jrtaylor95@gmail.com>
2018-03-28 16:33:03 -0400

    Revert "initial commit"
    This reverts commit 6385c1fa57c1cf244063f58a57f3921f66221a9a.

--- src/main/java/com/revature/util/HttpErrorMessages.java
+++
@@ -1,7 +0,0 @@
-package com.revature.util;
-
-public class HttpErrorMessages {
-	public static final String STATUS_400 = "400 BAD REQUEST";
-	public static final String STATUS_404 = "404 NOT FOUND";
-	public static final String STATUS_500 = "500 INTERNAL SERVER ERROR";
-}
